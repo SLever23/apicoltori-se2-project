10932d893a478abd29b6fcf5d52b873238fe4daa
Michele <michele.tessari12@gmail.com>
2018-11-27 22:53:58 +0100

    fix the tests for jest

--- routes/tasks/tasks_logic/tasks_logic.js
+++ routes/tasks/tasks_logic/tasks_logic.js
@@ -3,49 +3,55 @@ const _ = require('lodash');
 
 module.exports = {
     add_task: (task) => {
-        if (validate_create(task)) {
+        if (module.exports.validate_create(task)) {
             let new_index = db.tasks.length
             task.id = new_index;
             db.tasks.push(task);
-            return topic;
+            return task;
         } else {
             throw 'Invalid task';
         }
     },
     get_tasks: (searchkey) => {
-        //return task with the matching title, description, topic, type
+        if (searchkey != null && searchkey != undefined && (typeof searchkey) === 'string') {
+            return db.tasks.find(function (element) { return element.title.includes(searchkey) || element.description.includes(searchkey); });
+        }
+        else
+            throw 'invalid searchkey';
     },
     get_task_by_id: (id) => {
-        if(isNaN(id) || id < 0)
+        if (isNaN(id) || id < 0)
             throw 'id not valid';
         else
             return db.tasks[id];
     },
-    update_task: (task,id) => {
-        if(validate_update(task)){
+    update_task: (task, id) => {
+        if (module.exports.validate_update(task)) {
             db.tasks[id] = task; //da rivedere, non credo che funzioni
         }
         else
             throw 'invalid task';
     },
     delete_task: (id) => {
-    if(isNaN(id) || id < 0)
-        throw 'id not valid';
-    else;
+        if (isNaN(id) || id < 0)
+            throw 'id not valid';
+        else
+            return;
         //da rivedere, se lo elimino nel modo classico succede un disastro con gli id
+    },
+
+    validate_create: (task) => {//da finire
+        if (task == null || task == undefined || task.title == null || task.title == undefined || task.description == null || task.description == undefined || task.type == null || task.type == undefined)
+            return false;
+        else if (isFinite(task.id) && task.id >= 0 && task.title.length > 0 && task.description.length > 0 && task.type.length > 0)
+            return true;
+        else
+            return false;
+    },
+    validate_update: (task) => {// da finire
+        if (task == null || task == undefined)
+            return false;
+        else
+            return true;
     }
-}
-
-validate_create: (task) => {//da finire
-    if(task == null || task == undefined)
-        return false;
-    else
-        return true;
-}
-
-validate_update: (task) => {// da finire
-    if(task == null || task == undefined)
-        return false;
-    else
-        return true;
 }
