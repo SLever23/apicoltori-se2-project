02e2261683b828ccff62e1ab85c78f779e49bfd0
Simone Lever <simonelever@gmail.com>
2018-11-27 20:27:19 +0100

    implemented submission_logic POST and GET/id and fix test for it

--- routes/submissions/logic/submissions_logic.js
+++ routes/submissions/logic/submissions_logic.js
@@ -1,20 +1,52 @@
 const db = require('../../../db/db.js');
 const _ = require('lodash');
 
-module.exports = {
-    sumbission_get_all : (examId, userId, taskId) => {
-        //return array of submissions
-    },
-
-    submission_create: (submission) => {
-        return submission;
-    },
-
-    submission_get_by_id : (id) => {
-        //return submission;
-    },
-    
-    submission_edit: (submission) => {
+function validation_create_submission(submission) {
+    if (submission && Number.isInteger(submission.examId) && Number.isInteger(submission.userId) && Number.isInteger(submission.taskId) && typeof submission.response === 'string') {
+        if (_.find(db.exams, function (o) { return submission.examId == o.id })
+            && _.find(db.exams, function (o) { return submission.userId == o.id })
+            && _.find(db.exams, function (o) { return submission.taskId == o.id })) {
+            return true;
+        }
+        else
+            return false
+    }
+    else
         return false;
+}
+
+function submission_create(submission) {
+    if (validation_create_submission(submission)) {
+        submission.id = db.submissions.length;
+        db.submissions.push(submission);
+        return submission;
+    }
+    else {
+        throw 'Bad Request';
     }
 }
+
+function submission_get_by_id(id) {
+    if (Number.isInteger(id)) {
+        if (db.submissions[id] !== null && db.submissions[id] !== undefined) {
+            return db.submissions[id];
+        }
+        else
+            throw 'Not Found'
+    }
+    else {
+        throw 'Not Found'
+    }
+}
+
+function sumbission_get_all(examId, userId, taskId) {
+    //return array of submissions
+}
+
+function submission_edit(submission) {
+    return false;
+}
+
+
+module.exports = { validation_create_submission, submission_create, submission_get_by_id };
+
--- routes/submissions/logic/submissions_logic.test.js
+++ routes/submissions/logic/submissions_logic.test.js
@@ -1,64 +1,109 @@
 const submission_logic = require('./submissions_logic.js');
 const db = require('../../../db/db.js');
 
-test('Add invalid submissions', () => {
-    expect(submission_logic.submission_create(null)).toThrow();
-    
-    let submissions = {exam: "null", user: "ciao", task: "n", response: "cc"}
-    expect(submission_logic.submission_create(submissions)).toThrow();
+test('validation_create_submission', () => {
+    db.exams.push({ id: 0 })
+    db.users.push({ id: 0 })
+    db.tasks.push({ id: 0 })
 
-    submissions = {exam: 0, user: 0, task: 0, response: 32}
-    expect(submission_logic.submission_create(submissions)).toThrow();
+    let submission = { examId: 0, userId: 0, taskId: 0, response: "response" };
+
+    expect(submission_logic.validation_create_submission(submission)).toBe(true);
+});
+
+test('Add invalid submissions', () => {
+    function addnull() {
+        return submission_logic.submission_create(null);
+    }
+    expect(addnull).toThrow();
+
+    function addstring() {
+        return submission_logic.submission_create("prova");
+    }
+    expect(addstring).toThrow();
+
+    function addinvalidint() {
+        let submission = { exam: "null", user: "ciao", task: "n", response: "cc" }
+        return submission_logic.submission_create(submission)
+    }
+    expect(addinvalidint).toThrow();
+
+    function addinvalidstring() {
+        let submission = { exam: 0, user: 0, task: 0, response: 32 }
+        return submission_logic.submission_create(submission)
+    }
+    expect(addinvalidstring).toThrow();
 });
 
 test('Add submissions with invalid reference ', () => {
-    db.exams.push({id: 0})
-    db.users.push({id: 0})
-    db.tasks.push({id: 0})
+    db.exams.push({ id: 0 })
+    db.users.push({ id: 0 })
+    db.tasks.push({ id: 0 })
 
-    submissions = {exam: 222, user: 0, task: 0, response: "ciao"}
-    expect(submission_logic.submission_create(submissions)).toThrow();
+    function addwrongexam() {
+        let submission = { exam: 222, user: 0, task: 0, response: "ciao" }
+        return submission_logic.submission_create(submission);
+    }
+    expect(addwrongexam).toThrow();
 
-    submissions = {exam: 0, user: 222, task: 0, response: "ciao"}
-    expect(submission_logic.submission_create(submissions)).toThrow();
+    function addwronguser() {
+        let submission = { exam: 0, user: 222, task: 0, response: "ciao" }
+        return submission_logic.submission_create(submission);
+    }
+    expect(addwronguser).toThrow();
 
-    submissions = {exam: 0, user: 0, task: 222, response: "ciao"}
-    expect(submission_logic.submission_create(submissions)).toThrow();
+    function addwrongtask() {
+        let submission = { exam: 0, user: 0, task: 222, response: "ciao" }
+        return submission_logic.submission_create(submission);
+    }
+    expect(addwrongtask).toThrow();
 });
 
 test('Add valid submission ', () => {
-    db.exams.push({id: 0})
-    db.users.push({id: 0})
-    db.tasks.push({id: 0})
+    db.exams.push({ id: 0 })
+    db.users.push({ id: 0 })
+    db.tasks.push({ id: 0 })
 
-    submissions = {exam: 0, user: 0, task: 0, response: "ciao"}
-    let added = submission_logic.submission_create(submissions);
+    let submission = { examId: 0, userId: 0, taskId: 0, response: "response" };
+    let added = submission_logic.submission_create(submission);
 
-    expect(typeof(added)).toBe('object');
+    expect(typeof (added)).toBe('object');
     expect(added.id).toBe(db.submissions.length - 1);
 });
 
 test('Retrieve a submission that not exits or by wrong value', () => {
-    expect(submission_logic.submission_get_by_id(null)).toThrow();
+    function getnull() {
+        return submission_logic.submission_get_by_id(null)
+    }
+    expect(getnull).toThrow();
 
-    expect(submission_logic.submission_get_by_id('22')).toThrow();
+    function getnotint() {
+        expect(submission_logic.submission_get_by_id('22')).toThrow();
+    }
+    expect(getnotint).toThrow();
 
-    expect(submission_logic.submission_get_by_id(-23)).toThrow();
-})
+    function getintnotvalue() {
+        expect(submission_logic.submission_get_by_id(-23)).toThrow();
+    }
+    expect(getintnotvalue).toThrow();
+
+});
 
 test('Retrieve a submission that exits', () => {
-    db.submissions.push({id: 0, exam: 0, user: 0, task: 0, response: "ciao"})
-    
-    let result = submission_logic.submission_get_by_id(0);
-    expect(typeof(result)).toBe('object');
-    expect(result.id).toBe(0);
 
-    db.submissions.push({id: 1, exam: 0, user: 0, task: 0, response: "ciao"})
+    db.submissions[0]={ id: 0, exam: 0, user: 0, task: 0, response: "ciao" };
+
+    let result = submission_logic.submission_get_by_id(0);
+    expect(result.id).toBe(0);
+    expect(result.exam).toBe(0);
+    expect(result.user).toBe(0);
+    expect(result.response).toBe("ciao");
+
+    db.submissions.push({ id: 1, exam: 0, user: 0, task: 0, response: "ciao" })
 
     result = submission_logic.submission_get_by_id(1);
-    expect(typeof(result)).toBe('object');
     expect(result.id).toBe(db.submissions.length - 1);
-})
-
-
-
+    expect(result.exam).toBe(0);
+    expect(result.user).toBe(0);
+    expect(result.response).toBe("ciao");
+});
